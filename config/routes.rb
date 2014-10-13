88daa1009c834c3a96a77848ea5931aeec77b9bd
Adib Saad <adib.saad@gmail.com>
2014-10-13 04:09:09 -0400

    proper controller generation using versionist

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   api_version(:module => "V1", :path => {:value => "v1"}, :defaults => {:format => "json"}) do
-    get '/locations' => 'locations#index'
+    resources :locations
   end
   root "angular_templates#index"
   get '*path' => 'application#index'
